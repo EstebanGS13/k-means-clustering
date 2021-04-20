0432eb92a8c8df586379e590cb3176e3c1609827
EstebanGS13 <49171835+EstebanGS13@users.noreply.github.com>
2021-04-20 16:34:48 -0500

    Initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,9 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+    var range = 10
+    var random = scala.util.Random
+    // val k = random.nextInt(range) // TODO change back to random
+    val k = 3
+    println(k)
+  }
+}
