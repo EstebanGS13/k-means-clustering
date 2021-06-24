5b51e686181e3ab578e351220082637d2494511b
EstebanGS13 <49171835+EstebanGS13@users.noreply.github.com>
2021-06-24 13:32:43 -0500

    Replace for loops with map

--- src/main/scala/ParCollKMeans.scala
+++ src/main/scala/ParCollKMeans.scala
@@ -1,14 +1,13 @@
 package scala
 
-import scala.collection.mutable.{ArrayBuffer, Map}
 import scala.math.sqrt
 import scala.io.Source
 import scala.util.Random
 
 class ParCollKMeans(val K: Int, val dataset: String) {
   val data = loadData()
-  var clusters = initClusters()
   var centroids = initCentroids()
+  var clusters: Map[Int, Array[Array[Double]]] = null
   var error = 0.0
   val Epsilon = 0
 
@@ -23,10 +22,9 @@ class ParCollKMeans(val K: Int, val dataset: String) {
     var previous_sse = 0.0
     while (!end) {
       i += 1
-      emptyClusters()
 
-      // 2. & 3. Assign each point to their closest centroid
-      for (point <- data) assignPoint(point)
+      // 2. & 3. Assign each point to its closest centroid
+      calculateClusters()
 
       // 4. Update each cluster's centroid
       updateCentroids()
@@ -54,26 +52,11 @@ class ParCollKMeans(val K: Int, val dataset: String) {
     data.map(_.split(",").map(_.trim.toDouble)).toArray
   }
 
-  def initClusters(): Map[Int, ArrayBuffer[Array[Double]]] = {
-    /* Create a map of k-ArrayBuffers */
-    var clusters = Map.empty[Int, ArrayBuffer[Array[Double]]]
-    for (i <- 0 until K)
-      clusters += (i -> ArrayBuffer.empty[Array[Double]])
-    // HashMap(0 -> ArrayBuffer(), 1 -> ArrayBuffer(), 2 -> ArrayBuffer(), ...)
-
-    clusters
-  }
-
   def initCentroids(): Array[Array[Double]] = {
     /* Create an array of k-arrays, without specifying their dimension */
-    var centroids = Array.ofDim[Double](K, 0)
-    // Array(Array(), Array(), Array(), ...)
-
+    val centroids = Array.ofDim[Double](K, 0)
     // Fill the centroids array with random points from data
-    for (i <- 0 until K)
-      centroids(i) = randomCentroid(centroids)
-
-    centroids
+    centroids.map(i => randomCentroid(centroids))
   }
 
   def randomCentroid(
@@ -89,28 +72,17 @@ class ParCollKMeans(val K: Int, val dataset: String) {
     random.get
   }
 
-  def emptyClusters(): Unit = {
-    /* Empty each cluster to re-assign the points */
-    for ((k, v) <- clusters) clusters(k) = ArrayBuffer.empty[Array[Double]]
-  }
-
-  def assignPoint(point: Array[Double]): Unit = {
-    /* Assign a given point to the cluster of the closest centroid */
-    // Get index of the closest centroid
-    val centroidId = closestCentroid(point)
-    clusters(centroidId).append(point)
+  def calculateClusters(): Unit = {
+    /* Assign each point to its closest centroid */
+    clusters = data.groupBy(closestCentroid)
   }
 
   def closestCentroid(point: Array[Double]): Int = {
     /* Find the closest centroid to a given point and return its index */
-    var distances = new Array[Double](centroids.length)
     // Store the distances into an array for each centroid
-    for (i <- 0 until centroids.length)
-      distances(i) = euclideanDistance(point, centroids(i))
-
+    val distances = centroids.map(i => euclideanDistance(point, i))
     // Get the index of the shortest distance
-    val centroidId = distances.indexOf(distances.min)
-    centroidId
+    distances.indexOf(distances.min)
   }
 
   def euclideanDistance(
@@ -122,52 +94,28 @@ class ParCollKMeans(val K: Int, val dataset: String) {
   }
 
   def squaredDistance(point: Array[Double], centroid: Array[Double]): Double = {
-    var sum = 0.0
-    var sub = 0.0
-    for (i <- 0 until point.length) {
-      sub = point(i) - centroid(i)
-      sum += sub * sub
-    }
-    sum
+    (point zip centroid).map { case (p, c) =>
+      (p - c) * (p - c)
+    }.sum
   }
 
   def updateCentroids(): Unit = {
     /* Assign each cluster's mean value as their new centroid */
-    for ((centroidId, cluster) <- clusters) {
-      // If the cluster is empty, pick a random centroid
-      val newCentroid =
-        if (cluster.length < 2) randomCentroid() else mean(cluster)
-      centroids(centroidId) = newCentroid
+    centroids = (centroids zip clusters).map { case (centroid, (k, v)) =>
+      // If the cluster has only one value, pick a random centroid
+      if (v.length < 2) randomCentroid() else mean(v)
     }
   }
 
-  def mean(cluster: ArrayBuffer[Array[Double]]): Array[Double] = {
+  def mean(cluster: Array[Array[Double]]): Array[Double] = {
     /* Calculate the mean value of a given cluster of points */
-    // Create an array of zeros with the same length as the first point
-    var newCentroid = new Array[Double](cluster(0).length)
-
-    // Calculate the element wise sum of all the points
-    for (i <- 0 until cluster.length)
-      for (j <- 0 until cluster(i).length)
-        newCentroid(j) += cluster(i)(j)
-
-    // Divide each element of the new centroid by the number of points
-    for (i <- 0 until newCentroid.length)
-      newCentroid(i) /= cluster.length
-
-    newCentroid
+    cluster.transpose.map(_.sum).map(_ / cluster.length)
   }
 
   def calculateSse(): Double = {
-    var totalSum = 0.0
-    for ((centroidId, cluster) <- clusters) {
-      var sum = 0.0
-      for (point <- cluster)
-        sum += squaredDistance(point, centroids(centroidId))
-
-      totalSum += sum
-    }
-    totalSum
+    clusters.map { case (k, v) =>
+      v.map(i => squaredDistance(i, centroids(k))).sum
+    }.sum
   }
 
   def printInfo(printClusters: Boolean = false): Unit = {
